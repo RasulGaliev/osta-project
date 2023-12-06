f320f5a2b38d815b63ffb7711fc5638138a97872
seller <galiev.rasul.2002@yandex.ru>
2023-12-06 10:10:58 +0300

    project-types-component blocks and images

--- src/app/components/project-types/project-types.component.ts
+++ src/app/components/project-types/project-types.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class ProjectTypesComponent {
   buildIconUrl: string = './assets/images/project-types-page/project-types-block/icon/build.svg';
+
+  houseMainImgUrl: string = './assets/images/project-types-page/house-block/house-main.png';
+  houseGridImgUrl: string = './assets/images/project-types-page/house-block/house-grid.png';
+
+  malImg1Url: string = './assets/images/project-types-page/mall-block/mall1.png';
+  malImg2Url: string = './assets/images/project-types-page/mall-block/mall2.png';
+  malImg3Url: string = './assets/images/project-types-page/mall-block/mall3.png';
+
+  factoryMainImgUrl: string = './assets/images/project-types-page/factory-block/factory-main.png';
+  factoryGridImgUrl: string = './assets/images/project-types-page/factory-block/factory-grid.png';
+
+  interiorMainImgUrl: string = './assets/images/project-types-page/interior-block/interior-main.png';
+  interiorGridImgUrl: string = './assets/images/project-types-page/interior-block/interior-grid.png';
 }
