e542d8d2dccce6438fff0b7d5533ce0747d21f26
Sellers Helicent <sellersh@student.21-school.ru>
2023-12-10 17:05:55 +0300

    review

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {ReviewService} from "../../services/review.service";
+import {ReviewModel} from "../../models/review.model";
 
 @Component({
   selector: 'app-main',
@@ -7,6 +9,10 @@ import {ActivatedRoute} from "@angular/router";
   styleUrl: './main.component.css'
 })
 export class MainComponent {
+  reviews: ReviewModel[] = [];
+  currentIndex: number = 0;
+  currentIndex2: number = 1;
+
   sliderImgUrl: string = './assets/images/main-page/main-block/slider/slider1.png';
   titleImgUrl: string = './assets/images/main-page/main-block/main-title/main-title.svg';
 
@@ -15,15 +21,23 @@ export class MainComponent {
   aboutIcon2Url: string = './assets/images/main-page/about-block/icons/about-icon2.png';
   aboutIcon3Url: string = './assets/images/main-page/about-block/icons/about-icon3.png';
 
+  reviewLogoUrl: string = './assets/images/main-page/review-block/logo/logo.svg';
+
   contactPhoneIconUrl: string = './assets/images/main-page/contacts-block/contact-icons/phone.svg';
   contactEmailIconUrl: string = './assets/images/main-page/contacts-block/contact-icons/email.svg';
   contactMapIconUrl: string = './assets/images/main-page/contacts-block/contact-icons/map.svg';
   contactTelegramIconUrl: string = './assets/images/main-page/contacts-block/contact-icons/telegram.svg';
   contactWhatsAppIconUrl: string = './assets/images/main-page/contacts-block/contact-icons/whatsapp.svg';
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,  private reviewService: ReviewService) {}
 
   ngOnInit() {
+    this.reviews = this.reviewService.getReview();
+
+    setInterval(() => {
+      this.updateReviews()
+    }, 10000);
+    
     // Подписываемся на изменения в route.params
     this.route.fragment.subscribe(fragment => {
       // fragment содержит значение фрагмента в URL
@@ -38,6 +52,17 @@ export class MainComponent {
     const targetElement = document.getElementById('target');
     if (targetElement) {
       targetElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' });
+    }
+  }
+
+  updateReviews(): void {
+    this.currentIndex += 2;
+    this.currentIndex2 += 2;
+    if (this.currentIndex >= this.reviews.length) {
+      this.currentIndex = 0;
+    }
+    if (this.currentIndex2 >= this.reviews.length) {
+      this.currentIndex2 = 1;
     }
   }
 
---
+++ src/app/models/review.model.ts
@@ -0,0 +1,5 @@
+export interface ReviewModel {
+  id: number;
+  text: string;
+  author: string;
+}
