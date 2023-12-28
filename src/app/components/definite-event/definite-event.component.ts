80fb42b64e2dc04e01b02edd9fa0928414fb96d6
seller <galiev.rasul.2002@yandex.ru>
2023-12-28 13:12:43 +0300

    event+definite-event

---
+++ src/app/components/definite-event/definite-event.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-definite-event',
+  templateUrl: './definite-event.component.html',
+  styleUrl: './definite-event.component.css'
+})
+export class DefiniteEventComponent {
+
+}
